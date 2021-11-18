3cc25dd7c2ad604ba64f77929af6d679d8a777be
Jacquesm22 <jacquesmyburgh1999@gmail.com>
2021-11-18 21:27:32 +0200

    feat: added swagger

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-#TODO :(
+# API
+
+## Migrations
+```
+dotnet ef migrations add "InitialMigration" --project src\Infrastructure --startup-project src\WebApi --output-dir Persistence\Migrations
+```#TODO :(
---
+++ src/WebApi/Swagger/SwaggerFileOperationFilter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+
+namespace ExchangeAGram.WebApi.Swagger
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "NETREC Client Manager WebAPI v1", Version = "v1" });
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "NeTrecClientManager.WebApi.xml"));
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // must be lower case
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                options.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { securityScheme, Array.Empty<string>() }
+                });
+
+                options.OperationFilter<SwaggerFileOperationFilter>();
+            });
+
+            return services;
+        }
+    }
+}
